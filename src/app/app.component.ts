386d6ac45ac454b7f8011072a78077b043e22b3d
mertyldrr <mertyildirir14@gmail.com>
2023-06-18 11:47:29 +0200

    initialize the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase],
+})
+export class AppComponent {
+  title = 'hello-ng';
+}
