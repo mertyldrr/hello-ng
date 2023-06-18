b610c627a462f0b734652edd908862b31eba8300
mertyldrr <mertyildirir14@gmail.com>
2023-06-19 00:39:18 +0200

    add httpClient to app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
-})
-  .catch(err => console.error(err));
+  providers: [importProvidersFrom(BrowserModule), provideHttpClient()],
+}).catch((err) => console.error(err));
