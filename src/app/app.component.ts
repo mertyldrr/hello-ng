99582229657830767e8a0ad665ef4bddd1660b51
mertyldrr <mertyildirir14@gmail.com>
2023-06-20 22:58:54 +0200

    play video on start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import {
+  NgSwitch,
+  NgSwitchDefault,
+  NgSwitchCase,
+  CommonModule,
+} from '@angular/common';
+import { GameCardListComponent } from './game-card-list/game-card-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase],
+  imports: [
+    CommonModule,
+    NgSwitch,
+    NgSwitchDefault,
+    NgSwitchCase,
+    GameCardListComponent,
+    NavbarComponent,
+  ],
 })
 export class AppComponent {
   title = 'hello-ng';
+
+  videoEnded = false;
+
+  onVideoEnded(): void {
+    this.videoEnded = true;
+  }
 }
