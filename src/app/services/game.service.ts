8c7597a5e5d5e486f7f6d2133a64a420f38b9000
mertyldrr <mertyildirir14@gmail.com>
2023-06-20 22:59:47 +0200

    add service to fetch games from API

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Game, GameApiResponse } from '../game-card-list/game';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  apiKey = 'fc80409b7ac04531bb8ea0d5337189fe';
+  pageNumber = 1;
+  pageSize = 15;
+  apiUrl = `https://api.rawg.io/api/games?key=${this.apiKey}&page_size=${this.pageSize}`;
+  count: BehaviorSubject<number | undefined> = new BehaviorSubject<
+    number | undefined
+  >(undefined);
+  nextPageUrl: string | undefined;
+  prevPageUrl: string | undefined;
+  constructor(private http: HttpClient) {}
+
+  getGames(pageNumber: number = 1): Observable<Game[]> {
+    const url = `${this.apiUrl}&page=${pageNumber}`;
+    console.log(url);
+    return this.http.get<GameApiResponse>(url, httpOptions).pipe(
+      map((response) => {
+        this.count.next(response.count);
+        this.nextPageUrl = response.next;
+        this.prevPageUrl = response.previous;
+        return response.results.map((game) => ({
+          id: game.id,
+          background_image: game.background_image,
+          name: game.name,
+          released: game.released,
+          metacritic: game.metacritic,
+        }));
+      }),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  getCurrentPage(): number {
+    return this.pageNumber;
+  }
+
+  getCount(): Observable<number | undefined> {
+    return this.count.asObservable();
+  }
+
+  getNextPage(): void {
+    if (this.nextPageUrl) {
+      this.getGames(this.pageNumber + 1);
+      this.pageNumber += 1;
+    }
+  }
+
+  getPrevPage(): void {
+    if (this.prevPageUrl) {
+      this.getGames(this.pageNumber - 1);
+      this.pageNumber -= 1;
+    }
+  }
+
+  getLastPage(): Observable<number | undefined> {
+    return this.getCount().pipe(
+      map((count) => {
+        if (count) {
+          return Math.ceil(count / this.pageSize);
+        }
+        return undefined;
+      })
+    );
+  }
+}
