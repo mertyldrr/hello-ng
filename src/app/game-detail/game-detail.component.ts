e2241fc6ffb07661073464c0eb80af3d54607c4f
mertyldrr <mertyildirir14@gmail.com>
2023-06-22 13:30:32 +0200

    add game detail component[wip]

---
+++ src/app/game-detail/game-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-game-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game-detail.component.html',
+  styleUrls: ['./game-detail.component.css']
+})
+export class GameDetailComponent {
+
+}
